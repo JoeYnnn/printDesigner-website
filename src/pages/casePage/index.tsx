c3a4a51c1e81b167a94c9a0ded1e61c6c053e203
joe <q_yn@winning.com.cn>
2023-10-10 15:30:00 +0800

    1

--- src/pages/casePage/index.tsx
+++ src/pages/casePage/index.tsx
@@ -2,7 +2,7 @@ import {} from 'antd';
 import styles from './index.less';
 
 const index = () => {
-  return <div className={styles.sourcePage}></div>;
+  return <div className={styles.sourcePage}>11</div>;
 };
 
 export default index;
