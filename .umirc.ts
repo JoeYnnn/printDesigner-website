b0246247de9eb898408c56caa02d1a145482d55d
joe <q_yn@winning.com.cn>
2023-10-18 14:02:43 +0800

    11

--- .umirc.ts
+++ .umirc.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   ],
   title: '打印模版设计器',
   npmClient: 'yarn',
-  history: { type: 'hash' },
   base: '/web-wxp-print-portal',
   publicPath: '/web-wxp-print-portal/',
 });
